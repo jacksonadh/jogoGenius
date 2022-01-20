4ad9ded8035511bb224feb2d1c4e7651c0457412
Jackson Almeida <jacksonadh@gmail.com>
2022-01-20 19:54:20 -0300

    Separando projetos em repositorios distintos

--- githubusers/src/components/Button/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Button() {
-
-
-  return (
-    <>
-      <button type="submit">{ }</button>
-    </>
-  )
-};
--- githubusers/src/provider/providergit.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import api from "../services/api";
-
-export const gitcontext = createContext({
-  loading: false,
-  user: {},
-  repositorios: [],
-  starred: [],
-});
